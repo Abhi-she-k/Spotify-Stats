99402cd51f31620ceadc74b95efc2d4e967bb20e
Abhishek Paul <87787307+Abhi-she-k@users.noreply.github.com>
2025-03-22 13:30:13 -0400

    docker

--- functions/index.js
+++ functions/index.js
@@ -16,8 +16,8 @@ app.use(express.static(path.join(__dirname, '..', '/public')));
 
 global.access_token;
 const client_id = 'df40d135664a4a2cbae1c4db4de04977';
-// const local_host = 'http://localhost:3002'
-const local_host = 'https://spotifystats-kou6.onrender.com'
+const local_host = 'http://localhost:3002'
+// const local_host = 'https://spotifystats-kou6.onrender.com'
 const redirect_uri = local_host+'/callback';
 const client_secret = '5419800129b84a63920a4d20d12fbb6e'
 const scope = 'user-read-private user-read-email ugc-image-upload user-top-read user-library-read user-library-modify user-read-recently-played playlist-modify-private playlist-read-collaborative user-read-playback-state' 
@@ -132,9 +132,8 @@ app.get('/user',async function(req,res){
     
     const data = await getData('/me');
 
-    
     try{
-      res.json({name: data.display_name, pic: data.images[1].url, followers: data.followers.total})
+      res.json({name: data.display_name, pic: data.images[0].url, followers: data.followers.total})
       
     }
     catch{
